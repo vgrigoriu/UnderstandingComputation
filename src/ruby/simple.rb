9026d3786873043f12cd83dc96b7e8a8ab66853d
Victor Grigoriu <vgrigoriu@gmail.com>
2013-06-08 17:55:31 +0300

    Inspect expressions (ruby)

--- src/ruby/simple.rb
+++ src/ruby/simple.rb
@@ -1,13 +1,37 @@
+module Expression
+    def inspect
+        "«#{self}»"#.encoding
+    end
+end
+
 class Number < Struct.new(:value)
+    include Expression
+
+    def to_s
+        value.to_s
+    end
 end
 
 class Add < Struct.new(:left, :right)
+    include Expression
+
+    def to_s
+        "#{left} + #{right}"
+    end
 end
 
 class Multiply < Struct.new(:left, :right)
+    include Expression
+
+    def to_s
+        "#{left} * #{right}"
+    end
 end
 
 puts Add.new(
     Multiply.new(Number.new(1), Number.new(2)),
     Multiply.new(Number.new(3), Number.new(4))
-)
+).inspect
+
+puts Number.new(123).inspect
+
