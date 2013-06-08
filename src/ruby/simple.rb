dab543f8b7e701e3f0c46853f6c1983e9186991f
Victor Grigoriu <vgrigoriu@gmail.com>
2013-06-08 18:06:35 +0300

    Tests for inspect (ruby)

--- src/ruby/simple.rb
+++ src/ruby/simple.rb
@@ -1,3 +1,5 @@
+require "test/unit"
+
 module Expression
     def inspect
         "«#{self}»"#.encoding
@@ -28,10 +30,25 @@ class Multiply < Struct.new(:left, :right)
     end
 end
 
-puts Add.new(
-    Multiply.new(Number.new(1), Number.new(2)),
-    Multiply.new(Number.new(3), Number.new(4))
-).inspect
+class TestExpressions < Test::Unit::TestCase
+    def test_inspect_number
+        assert_equal('«14»', Number.new(14).inspect)
+    end
 
-puts Number.new(123).inspect
+    def test_inspect_add_numbers
+        add = Add.new(Number.new(12), Number.new(21))
+        assert_equal('«12 + 21»', add.inspect)
+    end
 
+    def test_inspect_multiply_numbers
+        multiply = Multiply.new(Number.new(17), Number.new(71))
+        assert_equal('«17 * 71»', multiply.inspect)
+    end
+
+    def test_inspect_complex_expression
+        expression = Add.new(
+            Multiply.new(Number.new(1), Number.new(2)),
+            Multiply.new(Number.new(3), Number.new(4)))
+        assert_equal('«1 * 2 + 3 * 4»', expression.inspect)
+    end  
+end
