1a5855258b2838096500bd769c19d9770d6841d9
Victor Grigoriu <vgrigoriu@gmail.com>
2013-08-06 21:23:07 +0300

    Implement if (page 37)

---
+++ src/c#/Simple.Tests/IfTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using Simple.Expressions;
+using Simple.Statements;
+using Xunit;
+
+namespace Simple.Tests
+{
+    public class IfTests
+    {
+        [Fact]
+        public void GivenAnIfStatement_WhenConditionIsTrue_ThenConsequenceIsExecuted()
+        {
+            var ifStatement = CreateIfStatement();
+
+            var environment = CreateBaseEnvironment();
+            environment = environment.AddValue("x", new Number(-1));
+
+            var machine = new Machine(ifStatement, environment);
+            machine.Run();
+
+            var finalEnvironment = machine.Environment;
+            finalEnvironment.GetValue<int>("a").Value.Should().Be(1, "because the consequence was executed");
+            finalEnvironment.GetValue<int>("b").Value.Should().Be(0, "because the alternative was not executed");
+        }
+
+        [Fact]
+        public void GivenAnIfStatement_WhenConditionIsFalse_ThenAlternativeIsExecuted()
+        {
+            var ifStatement = CreateIfStatement();
+
+            var environment = CreateBaseEnvironment();
+            environment = environment.AddValue("x", new Number(1));
+
+            var machine = new Machine(ifStatement, environment);
+            machine.Run();
+
+            var finalEnvironment = machine.Environment;
+            finalEnvironment.GetValue<int>("a").Value.Should().Be(0, "because the consequence was not executed");
+            finalEnvironment.GetValue<int>("b").Value.Should().Be(1, "because the alternative was executed");
+        }
+
+        private static IEnvironment CreateBaseEnvironment()
+        {
+            var environment = Environment.Empty;
+            environment = environment.AddValue("a", new Number(0));
+            environment = environment.AddValue("b", new Number(0));
+            return environment;
+        }
+
+        private static IStatement CreateIfStatement()
+        {
+            var condition = new LessThan(new Variable<int>("x"), new Number(0));
+            var consequence = new Assign<int>(new Variable<int>("a"), new Number(1));
+            var alternative = new Assign<int>(new Variable<int>("b"), new Number(1));
+
+            var ifStatement = new If(condition, consequence, alternative);
+            return ifStatement;
+        }
+    }
+}
---
+++ src/c#/Simple/Statements/If.cs
@@ -0,0 +1,40 @@
+﻿using Simple.Expressions;
+
+namespace Simple.Statements
+{
+    public class If : IStatement
+    {
+        private readonly IExpression<bool> condition;
+        private readonly IStatement consequence;
+        private readonly IStatement alternative;
+
+        public If(IExpression<bool> condition, IStatement consequence, IStatement alternative)
+        {
+            this.condition = condition;
+            this.consequence = consequence;
+            this.alternative = alternative;
+        }
+
+        public bool IsReducible
+        {
+            get { return true; }
+        }
+
+        public State Reduce(IEnvironment environment)
+        {
+            if (condition.IsReducible)
+            {
+                return new State(
+                    new If(condition.Reduce(environment), consequence, alternative),
+                    environment);
+            }
+
+            if (condition.Value)
+            {
+                return new State(consequence, environment);
+            }
+
+            return new State(alternative, environment);
+        }
+    }
+}
