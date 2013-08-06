d99e1556825c02dbae7c26df4557d2966ee821f0
Victor Grigoriu <vgrigoriu@gmail.com>
2013-08-06 21:40:24 +0300

    Implement sequence (page 39)

---
+++ src/c#/Simple.Tests/SequenceTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Simple.Expressions;
+using Simple.Statements;
+using Xunit;
+
+namespace Simple.Tests
+{
+    public class SequenceTests
+    {
+        [Fact]
+        public void GivenASequence_WhenItIsRun_BothStatementsAreExecuted()
+        {
+            var first = new Assign<int>(new Variable<int>("x"), new Add(new Number(1), new Number(2)));
+            var second = new Assign<int>(new Variable<int>("y"), new Add(new Number(3), new Number(4)));
+
+            var sequence = new Sequence(first, second);
+
+            var machine = new Machine(sequence, Environment.Empty);
+            machine.Run();
+            var finalEnvironment = machine.Environment;
+
+            finalEnvironment.GetValue<int>("x").Value.Should().Be(3, "the first statement was executed");
+            finalEnvironment.GetValue<int>("y").Value.Should().Be(7, "the second statement was executed");
+        }
+    }
+}
---
+++ src/c#/Simple/Statements/Sequence.cs
@@ -0,0 +1,32 @@
+﻿namespace Simple.Statements
+{
+    public class Sequence : IStatement
+    {
+        private readonly IStatement first;
+        private readonly IStatement second;
+
+        public Sequence(IStatement first, IStatement second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public bool IsReducible
+        {
+            get { return true; }
+        }
+
+        public State Reduce(IEnvironment environment)
+        {
+            if (first.IsReducible)
+            {
+                var stateAfterFirstReduction = first.Reduce(environment);
+                return new State(
+                    new Sequence(stateAfterFirstReduction.Program, second),
+                    stateAfterFirstReduction.Environment);
+            }
+
+            return new State(second, environment);
+        }
+    }
+}
