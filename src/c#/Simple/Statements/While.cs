3f9e30fdd4d4ca8f577a342aeef28da8706989ba
Victor Grigoriu <vgrigoriu@gmail.com>
2013-08-21 20:37:42 +0300

    Implement While (page 41)

---
+++ src/c#/Simple/Statements/While.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using Simple.Expressions;
+
+namespace Simple.Statements
+{
+    public class While : IStatement
+    {
+        private readonly IExpression<bool> condition;
+        private readonly IStatement body;
+
+        public While(IExpression<bool> condition, IStatement body)
+        {
+            this.condition = condition;
+            this.body = body;
+        }
+
+        public bool IsReducible
+        {
+            get { return true; }
+        }
+
+        public State Reduce(IEnvironment environment)
+        {
+            return new State(
+                new If(condition, new Sequence(body, this), new DoNothing()), 
+                environment);
+        }
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture, "while ({0}) {{ {1} }}", condition, body);
+        }
+    }
+}
