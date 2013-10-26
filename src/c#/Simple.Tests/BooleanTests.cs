427bb436bc84f187054cae19b030d30956c6ceff
Victor Grigoriu <vgrigoriu@gmail.com>
2013-10-26 18:06:46 +0100

    Add Boolean

---
+++ src/c#/Simple.Tests/BooleanTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Simple.Tests
+{
+    [TestFixture]
+    public class BooleanTests
+    {
+        [Test]
+        public void BooleanEvaluatesToSelf()
+        {
+            var sut = new Boolean(false);
+            var result = sut.Evaluate(null);
+
+            Assert.That(result, Is.SameAs(sut));
+        }
+
+        [Test]
+        public void BooleanValueIsTheOnePassedInConstructor()
+        {
+            var value = true;
+            var sut = new Boolean(value);
+
+            Assert.That(sut.Value, Is.EqualTo(value));
+        }
+    }
+}
---
+++ src/c#/Simple/Boolean.cs
@@ -0,0 +1,22 @@
+﻿namespace Simple
+{
+    public class Boolean: IExpression<bool>
+    {
+        private readonly bool value;
+
+        public bool Value
+        {
+            get { return value; }
+        }
+
+        public IExpression<bool> Evaluate(IEnvironment environment)
+        {
+            return this;
+        }
+
+        public Boolean(bool value)
+        {
+            this.value = value;
+        }
+    }
+}
