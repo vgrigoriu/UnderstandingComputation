f6a019dd9a760eaf430b0f118190ca45beb4ad48
Victor Grigoriu <vgrigoriu@gmail.com>
2013-10-27 14:01:27 +0200

    Add simple numbers

---
+++ src/c#/Simple.Tests/AddTests.cs
@@ -0,0 +1,22 @@
+﻿using Ploeh.AutoFixture.Xunit;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Simple.Tests
+{
+    public class AddTests
+    {
+        [Theory, AutoData]
+        public void AddAddsSimpleOperands(Number firstOperand, Number secondOperand)
+        {
+            var sut = new Add(firstOperand, secondOperand);
+
+            System.Console.WriteLine("{0} + {1}", firstOperand, secondOperand);
+
+            var result = sut.Evaluate(null);
+
+
+            Assert.Equal(firstOperand.Value + secondOperand.Value, result.Value);
+        }
+    }
+}
---
+++ src/c#/Simple/Add.cs
@@ -0,0 +1,23 @@
+﻿namespace Simple
+{
+    public class Add: IExpression<int>
+    {
+        private readonly Number firstOperand;
+        private readonly Number secondOperand;
+
+        public Add(Number firstOperand, Number secondOperand)
+        {
+            this.firstOperand = firstOperand;
+            this.secondOperand = secondOperand;
+        }
+        public int Value
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public IExpression<int> Evaluate(IEnvironment environment)
+        {
+            return new Number(firstOperand.Value + secondOperand.Value);
+        }
+    }
+}
