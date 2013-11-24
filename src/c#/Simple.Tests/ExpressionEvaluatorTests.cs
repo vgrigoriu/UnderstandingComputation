be16651f52e0243697ad28fe039f1fdc084a70b9
Victor Grigoriu <vgrigoriu@gmail.com>
2013-11-24 12:05:43 +0200

    Evaluate a number

---
+++ src/c#/Simple.Tests/ExpressionEvaluatorTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Simple.Tests
+{
+    public class ExpressionEvaluatorTests
+    {
+        [Fact]
+        public void CanEvaluateNumber()
+        {
+            var number = new Number(-123);
+            var sut = new ExpressionEvaluator<int>();
+
+            number.Accept(sut);
+
+            Assert.Equal(-123, sut.Value);
+        }
+    }
+}
---
+++ src/c#/Simple/ExpressionEvaluator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Simple
+{
+    public class ExpressionEvaluator<T>: IExpressionVisitor<T>
+    {
+        private T value;
+
+        public IExpressionVisitor<T> Visit(PrimitiveExpression<T> primitiveExpression)
+        {
+            value = primitiveExpression.Value;
+
+            return this;
+        }
+
+        public IExpressionVisitor<T> Visit(BinaryExpression<T> binaryExpression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IExpressionVisitor<T> Visit(Variable<T> variable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Value
+        {
+            get { return value; }
+        }
+    }
+}
